6d4f65d6ded1368a51af890418cec4e3878667cb
Noelle <noelle.teh@gmail.com>
2017-06-06 16:50:45 -0700

    updated some more stuff

--- Program.cs
+++ Program.cs
@@ -47,37 +47,90 @@ namespace cards
             
             //cards currently in play
             List<Card> inPlay = new List<Card>();
-
-            //begin player turn
+            bool gameIsGood = true;
             int playCount = 0;
-            int turn = playCount % players.Length;
-            Console.WriteLine($"{players[turn].name}, your turn! Ready? Press any key(s) to continue! And enter!");
-            Console.ReadLine();
-            //show current inplay deck (top 3 cards and total card count)
-            Console.Write("Top Cards: ");
-            for (int i = 0; i < inPlay.Count; i++) {
-                if (i == 3) { break; }
-                Console.Write(inPlay[inPlay.Count-i-1].ToString());
+            while (gameIsGood) {//begin player turn
+                int turn = playCount % players.Length;
+                Console.WriteLine($"{players[turn].name}, your turn! Ready? Press any key(s) to continue! And enter!");
+                Console.ReadLine();
+                //show current inplay deck (top 3 cards and total card count)
+                Console.Write("Top Cards: ");
+                for (int i = 0; i < inPlay.Count; i++) {
+                    if (i == 3) { break; }
+                    Console.Write("[" + inPlay[inPlay.Count-i-1].ToString() + "] ");
+                }
+                Console.WriteLine($"...({inPlay.Count} total cards in the pot)");
+                int cardIndex = 0;
+                int topVal;
+                if (inPlay.Count == 0) {topVal = 0;}
+                else {topVal = inPlay[inPlay.Count-1].val;}
+                bool playable = false;
+                foreach (Card c in players[turn].hand) {
+                    Console.WriteLine(cardIndex + " - " + c.ToString() + " ");
+                    cardIndex++;
+                    if(c.val > topVal || c.val == 3 || c.val == 7 || c.val == 10 )
+                    {
+                        playable = true;
+                    }
+                }
+                //future feature: players can play multiple cards if they have duplicates
+                //future feature: keep player hand sorted
+
+
+                //check if user has any playable cards
+
+                //prompt user for which card to play
+                if(playable)
+                {
+                    Console.WriteLine("Which card would you like to play?");
+                    //grab top value of cards in play
+                
+                    int r = playSelection(players[turn].hand.Count, topVal, players[turn].hand);
+                    //put this in an else
+                    Card playedCard = players[turn].hand[r];
+                    if(playedCard.val == 3)
+                    {
+                        players[turn].hand.Remove(playedCard);
+                        foreach(Card c in inPlay)
+                        {
+                            players[(playCount + 1) % 3].hand.Add(c);
+                        }
+                        System.Console.WriteLine("Skipping {0}'s turn.", players[(playCount + 1) % 3].name);
+                        System.Console.WriteLine(".... and they get all the cards HaHa!!.1");
+                        inPlay.Clear();
+                        playCount++;
+                    }
+                    else{
+                        inPlay.Add(players[turn].discard(r));
+                    }
+                   
+                    //redraw up to 3
+                    while (players[turn].hand.Count < 3) {
+                        if (d.cards.Count < 1) {
+                            break;
+                        }
+                        players[turn].draw(d);
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine("You can't play, have all the cards.");
+                    foreach(Card c in inPlay)
+                    {
+                        players[turn].hand.Add(c);
+                    }
+                    inPlay.Clear();
+                }
+                //if hand is empty and deck is empty, play from castle
+
+                //check if hand and castle are empty -- GAME OVER
+                playCount++;
+                if (playCount > 10) {
+                    gameIsGood = false;
+                }
             }
-            Console.Write($"...({inPlay.Count} total cards in the pot)");
-            foreach (Card c in players[turn].hand) {
-                int counter = 0;
-                Console.WriteLine(counter + " - " + c.ToString() + " ");
-                counter++;
-            }
-            //future feature: players can play multiple cards if they have duplicates
-            //prompt user for card to play
-            Console.WriteLine("Which card would you like to play?");
-            int topVal;
-            if (inPlay.Count == 0) {
-                topVal = 0;
-            }
-            else {
-                topVal = inPlay[inPlay.Count-1].val;
-            }
-            int r = playSelection(players[turn].hand.Count, topVal, players[turn].hand);
-            inPlay.Add(players[turn].discard(r));
-        }
+        
+        }//end main
 
         public static int playSelection(int numCards) {
             string response = Console.ReadLine();
@@ -95,7 +148,7 @@ namespace cards
                 return choice;
             }
             else if (playerCards[choice].val < topVal) {
-                System.Console.WriteLine("uh, no.");
+                System.Console.WriteLine("Uh, no. Try again.");
                 return playSelection(numCards, topVal, playerCards);
             }
             else {
